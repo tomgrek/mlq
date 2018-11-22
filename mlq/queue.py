23e66647a5c881cc41442654d4169eeaea50002e
Tom Grek <tom.grek@gmail.com>
2018-11-22 14:08:22 -0800

    Remove decode by default for different OSs.

--- mlq/queue.py
+++ mlq/queue.py
@@ -27,7 +27,7 @@ class MLQ():
         # msgs have a 64 bit id starting at 0
         self.id_key = self.q_name + '_max_id'
         self.id = str(uuid())
-        self._redis = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db, decode_responses=True)
+        self._redis = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db, decode_responses=False)
         logging.info('Connected to Redis at {}:{}'.format(redis_host, redis_port))
         self.pubsub = self._redis.pubsub(ignore_subscribe_messages=True)
         self.funcs_to_execute = []
