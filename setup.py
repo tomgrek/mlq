0c2fa8896d8c1ba4f47dc49747dff3580107a5fb
Tom Grek <tom.grek@gmail.com>
2018-11-22 14:41:44 -0800

    Fix requirements

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='mlq',
-    version='0.1.6',
+    version='0.1.8',
     packages=['mlq', 'controller'],
     long_description=open('README.txt').read(),
     install_requires=open('requirements.txt').read(),
+    include_package_data=True,
     url='https://github.com/tomgrek/mlq',
     author='Tom Grek',
     author_email='tom.grek@gmail.com'
